8c75259e4c4cc4a58536e00fb37ca0780876333f
miray kozlu <mikoz1@live.com>
2021-01-31 00:08:08 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //internal demek sadece entities ulaşabilir.
+    //public diğer katmanlarda erişebilsin.
+    //short int in bir küçüğü
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
