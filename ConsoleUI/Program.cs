ea9efeffe61366df8fe34ebaa22c4dde42c71fba
miray kozlu <mikoz1@live.com>
2021-02-03 23:47:22 +0300

    03.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,16 +1,19 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFrameWork;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //OPEN CLOSED PRİNCİPLE :Yaptığın yazılıma yeni bir özellik ekliyorsan mevcuttaki hiçbir koda dokunamazsın!!
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetByUnitPrice(50,100))
             {
                 Console.WriteLine(product.ProductName);
             }
---
+++ DataAccess/Concrete/EntityFrameWork/EfProductDal.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFrameWork
+{
+    public class EfProductDal : IProductDal
+    {
+        //NuGet
+        public void Add(Product entity)
+        {
+            //IDısposable pattern implementation of c# (using!Araştır)
+            using (NortwinContext context = new NortwinContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NortwinContext context = new NortwinContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (NortwinContext context  = new NortwinContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NortwinContext context = new NortwinContext())
+            {
+                return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (NortwinContext context = new NortwinContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
